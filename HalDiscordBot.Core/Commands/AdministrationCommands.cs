42448284e22d23f07a0236f35f91421d9a1706f4
Morphisor <a.gallitri@gmail.com>
2018-02-25 14:50:32 +0100

    switch to netcore libs instead of netstandard

---
+++ HalDiscordBot.Core/Commands/AdministrationCommands.cs
@@ -0,0 +1,24 @@
+﻿using Discord.Commands;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using System.Linq;
+using Discord;
+
+namespace HalDiscordBot.Core.Commands
+{
+    public class AdministrationCommands : CommandBase
+    {
+        [Command("prune")]
+        [Summary("Removes the last messages of the specified user")]
+        public async Task Prune([Remainder] [Summary("The user to prune")] string userName)
+        {
+            string userToPrune = !string.IsNullOrEmpty(userName) ? userName : "HAL";
+            var messages = await Context.Channel.GetMessagesAsync(20).ToList();
+            var transormed = messages.SelectMany(msg => msg);
+            var toDelete = transormed.Where(msg => msg.Author.Username == userToPrune);
+            await Context.Channel.DeleteMessagesAsync(toDelete);
+        }
+    }
+}
