88c4ed1b47a09613ac412dd0474a14c20d86f6b8
Morphisor <a.gallitri@gmail.com>
2018-04-16 15:01:32 +0200

    Added nsfw commands

---
+++ HalDiscordBot.Core/Commands/NSFWCommands.cs
@@ -0,0 +1,51 @@
+﻿using Discord.Commands;
+using HalDiscordBot.Rest;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HalDiscordBot.Core.Commands
+{
+    public class NSFWCommands : CommandBase
+    {
+        [Command("boob")]
+        [Summary("Random boobs")]
+        public async Task Boob()
+        {
+            var rng = new Random();
+            var restService = new RestService("http://api.oboobs.ru/boobs");
+            try
+            {
+                var response = await restService.MakeRequest($"/{rng.Next(10330)}");
+                JObject responseParsed = JObject.Parse(response);
+                var image = responseParsed["preview"];
+                await Context.Channel.SendMessageAsync($"http://media.oboobs.ru/{image}");
+            }
+            catch (Exception ex)
+            {
+                await HandleError("Error retrieving boobs", ex);
+            }
+        }
+
+        [Command("ass")]
+        [Summary("Random asses")]
+        public async Task Ass()
+        {
+            var rng = new Random();
+            var restService = new RestService("http://api.obutts.ru/butts/");
+            try
+            {
+                var response = await restService.MakeRequest($"/{rng.Next(4335)}");
+                JObject responseParsed = JObject.Parse(response);
+                var image = responseParsed["preview"];
+                await Context.Channel.SendMessageAsync($"http://media.obutts.ru/{image}");
+            }
+            catch (Exception ex)
+            {
+                await HandleError("Error retrieving Asses", ex);
+            }
+        }
+    }
+}
