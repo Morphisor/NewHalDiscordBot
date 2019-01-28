7ab201720ebcff2ca72bade271e56212c7dfbb65
Morphisor <a.gallitri@gmail.com>
2019-01-28 09:45:44 +0100

    added custom logic

---
+++ HalDiscordBot.Core/CustomLogic/UserUpdated/FedeUserUpdated.cs
@@ -0,0 +1,42 @@
+﻿using Discord.WebSocket;
+using HalDiscordBot.Core.CustomLogic.Bases;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace HalDiscordBot.Core.CustomLogic.UserUpdated
+{
+    public class FedeUserUpdated : UserUpdatedLogic
+    {
+        public FedeUserUpdated(ISocketMessageChannel currentChannel) : base(currentChannel)
+        {
+        }
+
+        public override void UserJoined(string user)
+        {
+            RemoveLog(user);
+        }
+
+        public override void UserLeft(string user)
+        {
+            RemoveLog(user);
+        }
+
+        public override void UserMoved(string userName, string from, string to)
+        {
+            if (from != "KickChannel" && to != "KickChannel")
+                RemoveLog(userName);
+        }
+
+        private void RemoveLog(string userName)
+        {
+            if(userName == "Federik")
+            {
+                var message = _currentChannel.GetMessagesAsync(1).ToList();
+                var transformed = message.GetAwaiter().GetResult().SelectMany(msg => msg);
+                var task = _currentChannel.DeleteMessagesAsync(transformed);
+                task.GetAwaiter().GetResult();
+            }
+        }
+    }
+}
