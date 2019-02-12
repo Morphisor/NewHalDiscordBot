5adff1defbd6655d712af8059da6d439d7294874
Morphisor <a.gallitri@gmail.com>
2019-02-12 17:31:13 +0100

    added custom logic to remove hal joining channel for audio commands

---
+++ HalDiscordBot.Core/CustomLogic/UserUpdated/HalAudioUserUpdated.cs
@@ -0,0 +1,46 @@
+﻿using Discord.WebSocket;
+using HalDiscordBot.Core.CustomLogic.Bases;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace HalDiscordBot.Core.CustomLogic.UserUpdated
+{
+    public class HalAudioUserUpdated : UserUpdatedLogic
+    {
+        public HalAudioUserUpdated(ISocketMessageChannel currentChannel) : base(currentChannel)
+        {
+        }
+
+        public override void UserJoined(string user)
+        {
+            RemoveLog(user);
+        }
+
+        public override void UserLeft(string user)
+        {
+            RemoveLog(user);
+        }
+
+        public override void UserMoved(string userName, string from, string to)
+        {
+            if (from != "KickChannel" && to != "KickChannel")
+                RemoveLog(userName);
+        }
+
+        private void RemoveLog(string userName)
+        {
+            if (userName == "HAL")
+            {
+                var message = _currentChannel.GetMessagesAsync(3).ToList();
+                var transformed = message.GetAwaiter().GetResult().SelectMany(msg => msg);
+                if(transformed.Any(msg => msg.Content.Contains("$a")))
+                {
+                    var hallogs = transformed.Where(msg => msg.Author.Username == "HAL");
+                    var task = _currentChannel.DeleteMessagesAsync(hallogs);
+                    task.GetAwaiter().GetResult();
+                }
+            }
+        }
+    }
+}
